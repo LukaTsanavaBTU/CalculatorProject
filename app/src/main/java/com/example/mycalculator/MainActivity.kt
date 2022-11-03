9c8eb7df4cb9615182d0fbcf9899a1aa006997fb
LukaTsanavaBTU <luka.tsanava.1@btu.edu.ge>
2022-11-03 19:16:08 +0400

    Equals Funcion Improvements

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     private var firstOperand = 0.0
     private var operator = ""
     private var operatorBeenPressed = false
+    private var equalsBeenPressed = false
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +26,10 @@ class MainActivity : AppCompatActivity() {
     fun clickNumber(clickedView: View) {
         if (clickedView is TextView) {
             if ((tvResult.text.toString() != "Infinity") and (tvResult.text.toString() != "NaN")) {
+                if (equalsBeenPressed) {
+                    tvResult.text = "0"
+                    equalsBeenPressed = false
+                }
                 var currentText = tvResult.text.toString()
                 val addedNumber = clickedView.text.toString()
 
@@ -41,6 +46,7 @@ class MainActivity : AppCompatActivity() {
 
     fun clickOperator(clickedView: View) {
         if (clickedView is TextView){
+            equalsBeenPressed = false
             if (tvResult.text.toString() != "") {
                 if (!operatorBeenPressed) {
                     firstOperand = tvResult.text.toString().toDouble()
@@ -70,7 +76,7 @@ class MainActivity : AppCompatActivity() {
 
     fun clickEquals(clickedView: View) {
         if (clickedView is TextView){
-            if (tvResult.text.toString() != "") {
+            if ((tvResult.text.toString() != "") and (operator != "")) {
                 var finalResult = 0.0
                 val secondOperand = tvResult.text.toString().toDouble()
                 when(operator){
@@ -81,12 +87,17 @@ class MainActivity : AppCompatActivity() {
                 }
                 if (finalResult.toString().slice(finalResult.toString().length - 2 until finalResult.toString().length) == ".0"){
                     tvResult.text = finalResult.toString().slice(0 .. finalResult.toString().length - 3)
+                    operator = ""
+                    firstOperand = 0.0
+                    operatorBeenPressed = false
+                    equalsBeenPressed = true
                 }
-                else { tvResult.text = finalResult.toString() }
+                else { tvResult.text = finalResult.toString()
+                    operator = ""
+                    firstOperand = 0.0
+                    operatorBeenPressed = false
+                    equalsBeenPressed = true}
 
-                operator = ""
-                firstOperand = 0.0
-                operatorBeenPressed = false
             }
         }
     }
@@ -97,12 +108,14 @@ class MainActivity : AppCompatActivity() {
             firstOperand = 0.0
             tvResult.text = "0"
             operatorBeenPressed = false
+            equalsBeenPressed = false
         }
     }
 
     fun clickBackspace(clickedView: View) {
         if (clickedView is TextView) {
             if ((tvResult.text.toString() != "Infinity") and (tvResult.text.toString() != "NaN")) {
+                equalsBeenPressed = false
                 if (tvResult.text.toString() != "0") {
                     tvResult.text = tvResult.text.toString().dropLast(1)
                 }
@@ -114,8 +127,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun clickDecimal(clickedView: View) {
-        if ((tvResult.text.toString() != "Infinity") and (tvResult.text.toString() != "NaN")) {
-            if (clickedView is TextView) {
+        if (clickedView is TextView){
+            if ((tvResult.text.toString() != "Infinity") and (tvResult.text.toString() != "NaN")) {
+                if (equalsBeenPressed) {
+                    tvResult.text = "0"
+                    equalsBeenPressed = false
+                }
                 if (tvResult.text.toString() == "") {
                     tvResult.text = "0."
                 } else if ("." !in tvResult.text.toString()) {
